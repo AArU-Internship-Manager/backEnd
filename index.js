d6723b9dc9b4ed724502588b3bdc0885c539d37c
ahmad <ahmad.hajqasem@hotmail.com>
2022-10-05 19:45:13 +0300

    add student api

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ app.use(cors())
 app.use(express.json());
 const loginRouter = require('./routes/loginApi')
 const adminRouter = require('./routes/adminApi')
+const studentRouter = require('./routes/studentApi')
 app.use('/login', loginRouter)
 app.use('/admin', adminRouter)
+app.use('/student', studentRouter)
 
 
 app.listen(port, () => {
