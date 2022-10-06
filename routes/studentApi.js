bb31ad2837f075293ee7e18350a7732d55918086
ahmad <ahmad.hajqasem@hotmail.com>
2022-10-06 09:46:19 +0300

    remove consol.log

--- routes/studentApi.js
+++ routes/studentApi.js
@@ -20,7 +20,6 @@ router.post('/', (req, res, next) => {
 
     const sql = `INSERT INTO student_e (full_name, nationality, university_id, collage, study_field, birth_place, gender, phone, email,  location, passport_id, Section, Scientific_level, health_status, study_year, total_study_year, fluency_in_English, total_hour, passport_end_date, birth_date)
      VALUES ("${full_name}", "${nationality}", ${university_id}, "${collage}", "${study_field}","${birth_place}","${gender}", ${phone}, "${email}","${location}", ${passport_id}, "${Section}", "${Scientific_level}","${health_status}", "${study_year}", "${total_study_year}", "${fluency_in_English}", ${total_hour}, "${passport_end_date}", "${birth_date}")`;
-    console.log(sql)
     pool.query(sql, (err, result) => {
         if (err) {
             res.status(404);
